0a76697a38a5fcf1b767460cdd390b92c0695609
Xaucos <130907231+Xaucos@users.noreply.github.com>
2023-04-30 10:59:45 +0300

    v0.3

--- PYTIN.py
+++ PYTIN.py
@@ -7,3 +7,22 @@ txt_instruction = ('Данное приложение похволит вам с
                    'Проба Руфье представляет собой нагрузочной комплекс,предзназначенный для работоспсобности сердца при нагрузке.\n'
                    'у испытуемого,
                    'У испытуемого, в состояние л  жа на спине в течение 5 минут,опредалять пульс в течении 15секунд:\n
+txt_title = 'Программа сдохни или умри'
+txt_name = 'Введите имя'   
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = ''
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''                   
